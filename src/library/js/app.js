4827ef525f3fc239e9212812d5de6f4438dcc9cd
schattaraj <schattaraj200@gmail.com>
2025-02-03 20:34:47 +0530

    update

--- src/library/js/app.js
+++ src/library/js/app.js
@@ -35,11 +35,11 @@ var swiper1 = new Swiper(".mySwiper", {
 // }
 // window.addEventListener("online",refresh);
 
-document.addEventListener("visibilitychange", () => {
-  if (document.visibilityState === 'visible') {
-    refresh(); // Reloads the page when the document becomes visible
-  }
-});
+// document.addEventListener("visibilitychange", () => {
+//   if (document.visibilityState === 'visible') {
+//     refresh(); // Reloads the page when the document becomes visible
+//   }
+// });
 
 new kursor({
   type: 4,
